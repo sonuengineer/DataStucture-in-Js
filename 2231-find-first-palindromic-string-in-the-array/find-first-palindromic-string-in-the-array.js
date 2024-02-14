a00f1c8af3b248364c1d47f90c77b6aea6c9fe01
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-02-14 16:16:02 +0530

    Time: 70 ms (75.81%) | Memory: 55.9 MB (38.67%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+       for (let word of words){
+              if(word===word.split("").reverse().join("")){
+                return word 
+              }
+       }
+       return ""
+};
