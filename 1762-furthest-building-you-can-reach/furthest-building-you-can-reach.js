9595c592baec5dff8ba2020406060eb66fd6dcd8
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-02-17 17:04:10 +0530

    Time: 97 ms (77.78%) | Memory: 65.3 MB (42.59%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.js
@@ -0,0 +1,87 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] > this.heap[index]) {
+                [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+
+        const minValue = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return minValue;
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild < element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if ((swap === null && rightChild < element) || (swap !== null && rightChild < leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (swap === null) break;
+
+            this.heap[index] = this.heap[swap];
+            this.heap[swap] = element;
+            index = swap;
+        }
+    }
+}
+
+var furthestBuilding = function(heights, bricks, ladders) {
+    const n = heights.length;
+    const heap = new MinHeap();
+
+    for (let i = 0; i < n - 1; i++) {
+        const heightDiff = heights[i + 1] - heights[i];
+        if (heightDiff > 0) {
+            heap.insert(heightDiff);
+            if (heap.heap.length > ladders) {
+                bricks -= heap.extractMin();
+            }
+            if (bricks < 0) {
+                return i;
+            }
+        }
+    }
+
+    return n - 1;
+};
+
+
+
