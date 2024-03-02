03bad5dc786d553f4c7b5ed8d15ba93f96be311f
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-03-02 10:57:12 +0530

    Time: 77 ms (46.15%) | Memory: 52.8 MB (25.84%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x<2) return x
+    let left=0,right=x/2
+    while(left<=right){
+        const mid=Math.floor((left+right)/2)
+        if(mid*mid<=x && x<(mid+1)*(mid+1)){
+            return mid
+        }else if((mid*mid)>x){
+            right=mid-1
+        }else{
+            left=mid+1
+        }
+    }
+    return -1
+};
