37aaaf64bf0f3ba513b70da95b56f8b8d8a147e4
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-03-14 14:59:53 +0530

    Time: 45 ms (92.03%) | Memory: 49.2 MB (37.21%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.js
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.js
@@ -1,25 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var findMin = function(nums) {
-    if (nums.length === 1) {
-        return nums[0];
-    }
-
-    if (nums.length === 2) {
-        if (nums[0] > nums[1]) {
-            return nums[1];
-        }
-        return nums[0];
-    }
-
-    const middleIndex = Math.floor(nums.length / 2);
-    const middleNumber = nums[middleIndex];
-    const lastNumber = nums[nums.length - 1];
-
-    if (middleNumber > lastNumber) {
-        return findMin(nums.splice(middleIndex));
+    var left = 0;
+  var right = nums.length - 1;
+  var mid = 0;
+  while (left < right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else if (nums[mid] < nums[right]) {
+      right = mid;
     } else {
-        if (middleNumber === lastNumber) {
-            return Math.min(findMin(nums.slice(middleIndex)), findMin(nums.slice(0, middleIndex)));
-        }
-        return findMin(nums.splice(0, middleIndex + 1));
+      right--;
     }
+  }
+  return nums[left];
 };
