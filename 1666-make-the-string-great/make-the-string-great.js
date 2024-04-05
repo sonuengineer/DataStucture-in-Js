07bf2bfa36906b01703fb4e7eed36de9d6b319c2
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-04-05 10:08:26 +0530

    Time: 51 ms (89.41%) | Memory: 50.3 MB (91.22%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -0,0 +1,13 @@
+var makeGood = function(s) {
+    const stack = [];
+    
+    for (const char of s) {
+        if (stack.length > 0 && Math.abs(char.charCodeAt() - stack[stack.length - 1].charCodeAt()) === 32) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.join('');
+};
