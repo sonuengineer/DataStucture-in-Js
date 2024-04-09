60ab2f278c5bc302744c52df4d368557429eff02
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-04-09 18:28:08 +0530

    Time: 44 ms (91.82%) | Memory: 48.6 MB (75.14%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.js
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.js
@@ -1,13 +1,11 @@
-/**
- * @param {number[]} gain
- * @return {number}
- */
-var largestAltitude = function(gain) {
-    let current=0
-    let maxh=0
-   for(let i=0;i<gain.length;i++){
-         current+=gain[i]
-         maxh=Math.max(maxh,current)
-   }
-    return maxh
+const largestAltitude = gain => {    
+    let previous = 0;
+    let biggest = 0;
+    
+    for (let i = 0; i < gain.length; i += 1) {
+        previous += gain[i];
+        if (previous > biggest) biggest = previous;
+    }
+    
+    return biggest;
 };
