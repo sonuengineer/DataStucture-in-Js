4fb8f0836c65245ba99c56b5f3fad2b5c34fbaca
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-04-13 09:31:56 +0530

    Time: 61 ms (18.17%) | Memory: 48.9 MB (43.68%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -0,0 +1,13 @@
+var timeRequiredToBuy = function(tickets, k) {
+    let total = 0;
+
+    for (let i = 0; i < tickets.length; i++) {
+        if (i <= k) {
+            total += Math.min(tickets[i], tickets[k]);
+        } else {
+            total += Math.min(tickets[i], tickets[k] - 1);
+        }
+    }
+
+    return total;
+};
