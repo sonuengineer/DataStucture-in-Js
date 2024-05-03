e2d090716949bdf6769866c0d45f4b519b0aae8c
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-05-03 09:31:35 +0530

    Time: 914 ms (53.85%) | Memory: 95.2 MB (30.77%) - LeetSync

---
+++ 746-prefix-and-suffix-search/prefix-and-suffix-search.js
@@ -0,0 +1,19 @@
+var WordFilter = function (words) {
+  this.map = new Map();
+  for (let i = 0; i < words.length; i++) {
+    let prefix = '';
+    for (let j = 0; j <= words[i].length; j++) {
+      let suffix = '';
+      for (let k = 0; k <= words[i].length; k++) {
+        suffix = words[i].slice(k);
+        this.map.set(prefix + '#' + suffix, i);
+      }
+      prefix += words[i][j];
+    }
+  }
+}
+
+WordFilter.prototype.f = function (prefix, suffix) {
+  let target = prefix + '#' + suffix;
+  return this.map.has(target) ? this.map.get(target) : -1;
+}
