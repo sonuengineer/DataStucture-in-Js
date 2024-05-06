437dfff2d5f2b66359d7cb1599e34902f1082d0f
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-05-06 09:44:56 +0530

    Time: 294 ms (41.82%) | Memory: 84.5 MB (100.00%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.js
@@ -0,0 +1,22 @@
+var removeNodes = function(head) {
+    let cur = head;
+    let stack = [];
+    
+    while (cur !== null) {
+        while (stack.length > 0 && stack[stack.length - 1].val < cur.val) {
+            let a = stack.pop();
+        }
+        stack.push(cur);
+        cur = cur.next;
+    }
+    
+    let nxt = null;
+    while (stack.length > 0) {
+        cur = stack.pop();
+        cur.next = nxt;
+        nxt = cur;
+    }
+    
+    return cur;
+
+};
