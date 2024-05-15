595a699709190ecac687cd152799058dc00f9058
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-05-15 09:34:52 +0530

    Time: 64 ms (23.81%) | Memory: 50.3 MB (47.62%) - LeetSync

---
+++ 1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.js
@@ -0,0 +1,17 @@
+var minSwaps = function (s) {
+    let zeros = s.replaceAll('1', '').length;
+    let ones = s.length - zeros;
+
+    if (Math.abs(zeros - ones) > 1) return -1;
+    function swapString(curr) {
+        let swaps = 0;
+        for (let char of s) {
+            if (char !== curr) swaps += 1;
+            curr = `${curr ^ 1}`;
+        }
+        return swaps / 2;
+    }
+    if (zeros > ones) return swapString('0');
+    if (zeros < ones) return swapString('1');
+    return Math.min(swapString('0'), swapString('1'));
+};
