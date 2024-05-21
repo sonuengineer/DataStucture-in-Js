ba70487e05628da7b401d1157b0b2d9a56f22506
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-05-21 14:10:11 +0530

    Time: 93 ms (84.98%) | Memory: 61.7 MB (83.38%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -1,24 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var productExceptSelf = function(nums) {
     const n = nums.length;
-    const left = new Array(n).fill(1);
-    const right = new Array(n).fill(1);
-
+    
+    const prefix = new Array(n).fill(1);
+    const suffix = new Array(n).fill(1);
+    
     for (let i = 1; i < n; i++) {
-        left[i] = left[i - 1] * nums[i - 1];
+        prefix[i] = prefix[i - 1] * nums[i - 1];
     }
-
+    
     for (let i = n - 2; i >= 0; i--) {
-        right[i] = right[i + 1] * nums[i + 1];
+        suffix[i] = suffix[i + 1] * nums[i + 1];
     }
-
-    const result = [];
+    
+    const answer = [];
     for (let i = 0; i < n; i++) {
-        result[i] = left[i] * right[i];
+        answer[i] = prefix[i] * suffix[i];
     }
-
-    return result;
+    
+    return answer;
 };
