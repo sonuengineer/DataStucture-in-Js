e46b111dfbbfa9d5a1be680bce0fe57dec2bf8e5
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-05-25 13:52:00 +0530

    Time: 73 ms (49.70%) | Memory: 60.1 MB (36.69%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -1,21 +1,22 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
- */
 var getCommon = function(nums1, nums2) {
-   let i = 0;
-    let j = 0;
+    // Sort both arrays for efficient comparison
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    
+    let common = -1; // Initialize common variable to track the common value
+    let i = 0, j = 0;
 
+    // Loop until one of the arrays ends
     while (i < nums1.length && j < nums2.length) {
-        if (nums1[i] === nums2[j]) {
-            return nums1[i];
-        } else if (nums1[i] < nums2[j]) {
-            i++;
-        } else {
-            j++;
+        if (nums1[i] === nums2[j]) { // If a common value is found
+            common = nums1[i]; // Update the common variable
+            break; // Break out of the loop since we found the minimum common value
+        } else if (nums1[i] < nums2[j]) { // If current value in nums1 is smaller
+            i++; // Move to the next element in nums1
+        } else { // If current value in nums2 is smaller
+            j++; // Move to the next element in nums2
         }
     }
 
-    return -1; //
+    return common; // Return the minimum common value
 };
