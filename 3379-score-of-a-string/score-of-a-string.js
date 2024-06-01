8ee9dee4deb75c7d43f3691134ffd9972c21de63
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-06-01 09:37:37 +0530

    Time: 55 ms (82.28%) | Memory: 49.1 MB (95.81%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -0,0 +1,7 @@
+var scoreOfString = function(s) {
+    let res = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        res += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+    }
+    return res;
+};
