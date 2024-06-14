e8d3c74426bcfcde1d17d8603b9e872709d70d2b
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-06-14 09:37:07 +0530

    Time: 80 ms (84.62%) | Memory: 55.3 MB (78.02%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.js
@@ -0,0 +1,15 @@
+var shiftGrid = function (grid, k) {
+  var arr = grid.flat(),
+    len = grid[0].length,
+    res = [];
+
+  while (k--) {
+    arr.unshift(arr.pop());
+  }
+
+  while (arr.length) {
+    res.push(arr.splice(0, len));
+  }
+
+  return res;
+};
