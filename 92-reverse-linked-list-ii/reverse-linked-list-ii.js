ab4036e156b31e8d6128248ea38f40f5e829ba67
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2024-06-28 10:11:18 +0530

    Time: 46 ms (88.59%) | Memory: 49.6 MB (7.16%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.js
@@ -0,0 +1,22 @@
+var reverseBetween = function(head, left, right) {
+    if (!head || left === right) return head;
+    
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+    
+    for (let i = 0; i < left - 1; ++i) {
+        prev = prev.next;
+    }
+    
+    let current = prev.next;
+    
+    for (let i = 0; i < right - left; ++i) {
+        const nextNode = current.next;
+        current.next = nextNode.next;
+        nextNode.next = prev.next;
+        prev.next = nextNode;
+    }
+    
+    return dummy.next;
+};
